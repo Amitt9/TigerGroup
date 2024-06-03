2d487adc68d07e3b99856783bd39a84588c8a8dd
Amitt9 <amithube25@gmail.com>
2024-06-03 23:50:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center h-screen items-center bg-gradient-to-r from-slate-400 to-orange-500 ">
+     <h1 className=" font-bold text-6xl  ">Welcome </h1>
+    </div>
+  );
+}
+
+export default App;
